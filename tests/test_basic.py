2ea0ef4780be26bc833202b5841a1bf48cfe2511
Sergio Rojas-Aguilar <128770839+Sergrojas29@users.noreply.github.com>
2026-03-15 19:48:16 -0400

    test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,11 @@
 import unittest
-import app.database
 from init_db import setup_database, teardown_database
 
 
 from app.database import SessionLocal
 from app.models.user import User
 
-
+#!  python -m unittest discover
 class TestAppBasic(unittest.TestCase):
     
     
@@ -19,6 +18,19 @@ class TestAppBasic(unittest.TestCase):
     def test_user(self)->None:
         with SessionLocal() as session:
             new_user = User(
-                name= , 
-        
+                name= "Sergio Rojas", 
+                role= "Is this need?",
+                # isadmin -> default false
+                username= "serg", #need email validation
+                email = "serg@gmail.com",
+                password= "Something"
             )
+            session.add(new_user)
+            
+            session.commit()
+            
+            fetch_user = session.query(User).all()
+            for user in fetch_user:
+                print(user.name)
+                print(user.email)
+                print(user.role)
