660b6421439fe0c8cabd2f92965da311e8cb59d2
Sergio Rojas-Aguilar <128770839+Sergrojas29@users.noreply.github.com>
2026-03-22 19:14:59 -0400

    update to deploy to render

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change'
-    db_database = "postgresql://dblab_9bmz_user:XnKfbcWDrTof7ZaH3LqNTQP0Or2n2qHN@dpg-d6rdfbnpm1nc73bhv58g-a/dblab_9bmz"
-    
-    # LOCAL POSTGRESQL  test
-    # db_database = ""postgresql://postgres:0415@localhost:5432/postgres""
-    
-
+    pass
 class DevelopmentConfig(Config):
     DEBUG = True
 
